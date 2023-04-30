67600d14490867f6568b4ed56eaf3c08361ec7b3
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-04-30 15:08:40 +0300

    fixed audio

---
+++ src/modules/props.js
@@ -0,0 +1,10 @@
+const props = {
+  counter: 0,
+  timeInterval: 10,
+  timeoutEnemy: 2000,
+  startPositionY: 200,
+  startPositionX: 300,
+  intervalId: 0,
+};
+
+export default props;
---
+++ src/modules/refs.js
@@ -0,0 +1,15 @@
+const refs = {
+  gameEl: document.querySelector('.game'),
+  targetEl: document.querySelector('.target'),
+  heroEl: document.querySelector('.hero'),
+  counterEl: document.querySelector('.counter'),
+  scoreValEl: document.querySelector('.score-val'),
+  backdropEl: document.querySelector('.backdrop'),
+  modalStartEl: document.querySelector('[data-modal="1"]'),
+  modalEndEl: document.querySelector('[data-modal="2"]'),
+  btnStartEl: document.querySelector('.btn-start'),
+  btnRetryEl: document.querySelector('.btn-retry'),
+  soundBoxEl: document.querySelector('.sound-box'),
+};
+
+export default refs;
